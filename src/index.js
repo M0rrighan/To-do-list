8888e30d53925d75eb484b7f9f2cc434a28e161f
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2021-09-22 17:00:24 +0200

    Add array of 'task' objects

--- src/index.js
+++ src/index.js
@@ -2,7 +2,25 @@ import './style.css';
 
 require('webpack-icons-installer/google.js');
 
-console.log('Hello');
 const tasksList = document.querySelector('.tasks_list');
+const TASKS = [
+  {
+    index: 0,
+    description: 'First task',
+    completed: false,
+  },
+  {
+    index: 1,
+    description: 'Second task',
+    completed: false,
+  },
+  {
+    index: 2,
+    description: 'Third task',
+    completed: false,
+  },
+];
+
+console.log(TASKS);
 
 tasksList.innerHTML = '<li>First item in list</li>';
