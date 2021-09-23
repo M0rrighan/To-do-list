49fd20be98cdef69b87695870175166e9426e6a1
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2021-09-23 02:16:30 +0200

    Add functions to sort array of Tasks and render on page load

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './style.css';
 
 require('webpack-icons-installer/google.js');
 
-// const tasksList = document.querySelector('.tasks_list');
+const tasksList = document.querySelector('.tasks_list');
 const TASKS = [
   {
     index: 0,
@@ -10,15 +10,41 @@ const TASKS = [
     completed: false,
   },
   {
-    index: 1,
-    description: 'Second task',
-    completed: false,
-  },
-  {
     index: 2,
     description: 'Third task',
     completed: false,
   },
+  {
+    index: 1,
+    description: 'Second task',
+    completed: false,
+  },
 ];
 
-// tasksList.innerHTML = '<li>First item in list</li>';
+function sortAscending(list, parametar = 'index') {
+  const arrayToSort = [...list];
+  return arrayToSort.sort((a, b) => (a[parametar] > b[parametar] ? 1 : -1));
+}
+
+function createLi(task) {
+  const { index, description, completed } = task;
+  const li = document.createElement('li');
+  li.id = index;
+  li.innerHTML = `
+  <input data-done="${completed}" 
+         data-referTo="${index}" 
+         type="checkbox" 
+         value="false" 
+         name="TaskCompleted" 
+         class="checkbox">
+  <p data-referTo="${index}" class="description">${description}</p>
+  <i data-referTo="${index}" class="material-icons">more_vert</i>
+  `;
+  return li;
+}
+
+window.onload = () => {
+  sortAscending(TASKS).forEach((task) => {
+    tasksList.appendChild(createLi(task));
+  });
+};
