dc8a72bd55bd1194f046a501db3bfb0e76fddf66
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2021-09-23 19:24:25 +0200

    Import neccessary classes; create new List; call function to populate HTML; Remove unneccessary code from index.js

--- src/index.js
+++ src/index.js
@@ -1,48 +1,6 @@
+import ListOfTasks from './list';
+import Interact from './interact';
 import './style.css';
 
-const tasksList = document.querySelector('.tasks_list');
-const TASKS = [
-  {
-    index: 0,
-    description: 'First task',
-    completed: false,
-  },
-  {
-    index: 2,
-    description: 'Third task',
-    completed: false,
-  },
-  {
-    index: 1,
-    description: 'Second task',
-    completed: false,
-  },
-];
-
-function sortAscending(list, parametar = 'index') {
-  const arrayToSort = [...list];
-  return arrayToSort.sort((a, b) => (a[parametar] > b[parametar] ? 1 : -1));
-}
-
-function createLi(task) {
-  const { index, description, completed } = task;
-  const li = document.createElement('li');
-  li.id = index;
-  li.innerHTML = `
-  <input data-done="${completed}" 
-         data-referTo="${index}" 
-         type="checkbox" 
-         value="false" 
-         name="TaskCompleted" 
-         class="checkbox">
-  <p data-referTo="${index}" class="description">${description}</p>
-  <i data-referTo="${index}" class="material-icons">more_vert</i>
-  `;
-  return li;
-}
-
-window.onload = () => {
-  sortAscending(TASKS).forEach((task) => {
-    tasksList.appendChild(createLi(task));
-  });
-};
+const tasks = new ListOfTasks();
+Interact.populateUlTasksList(tasks);
