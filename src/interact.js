91395fc8a06db6fabf441a05ca0b1f60ee0ecafd
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2021-09-24 14:31:18 +0200

    class Interact: Add functions: listenForNewItems(), listenClearBtn()

--- src/interact.js
+++ src/interact.js
@@ -1,11 +1,11 @@
-// eslint-disable-next-line import/no-cycle
+/* eslint-disable import/no-cycle */
 import Elmnts from './elements.js';
+import ListOfTasks from './list.js';
 
 export default class Interact {
   static populateUlTasksList(list) {
     this.tasksList = document.querySelector('.tasks_list');
     this.tasksList.innerHTML = '';
-
     list.getListSorted().forEach((task) => {
       const listItem = Elmnts.createLi(task, list);
       if (task.done) {
@@ -14,6 +14,30 @@ export default class Interact {
         listItem.classList.remove('done');
       }
       this.tasksList.appendChild(listItem);
+    });
+  }
+
+  static listenForNewItems() {
+    const addInput = document.getElementById('add_item');
+    addInput.addEventListener('keypress', (e) => {
+      if (e.key === 'Enter') {
+        new ListOfTasks().addToList(addInput.value);
+        addInput.value = '';
+      }
+    });
+
+    const addIcon = document.querySelector('.oneMoreTask i');
+    addIcon.addEventListener('click', () => {
+      new ListOfTasks().addToList(addInput.value);
+      addInput.value = '';
+    });
+  }
+
+  static listenClearBtn() {
+    const clearBtn = document.querySelector('.clearBtn');
+    clearBtn.addEventListener('click', (e) => {
+      console.log(e.target);
+      new ListOfTasks().removeAllDone();
     });
   }
 }
