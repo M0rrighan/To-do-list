1470eea46f959fb68715a80c6ad0342aa90f485b
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2021-09-24 17:40:31 +0200

    Edit class Elements, add EventListener-click- to <li> to show RemoveButton; eventListener-click- to removeButton

--- src/elements.js
+++ src/elements.js
@@ -52,6 +52,26 @@ export default class Elmnts {
     li.appendChild(ckbox);
     li.appendChild(descriptiveP);
     li.appendChild(icon);
+    li.addEventListener('click', () => {
+      // reset style for all list items
+      const allLis = document.querySelectorAll('.tasks_list > *');
+      allLis.forEach((li) => {
+        li.style.backgroundColor = 'white';
+        li.children[2].textContent = 'more_vert';
+        li.children[2].style.cursor = 'move';
+      });
+
+      // set style only for clicked
+      li.style.backgroundColor = 'hsl(40deg 91% 75% / 75%)';
+      icon.textContent = 'delete_forever';
+      icon.style.cursor = 'pointer';
+
+      icon.addEventListener('click', () => {
+        if (icon.textContent === 'delete_forever') {
+          listToUpdate.removeByIndex(parseInt(icon.dataset.referTo, 10));
+        }
+      });
+    });
     return li;
   }
 }
