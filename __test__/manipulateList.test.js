c25f2fda634d9a12fa948a94c532afc47a447f7d
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2021-09-30 15:23:01 +0200

    Add test group, to check if #addToList is working properly when list is initialized by passing a fake storage key name in the constructor method

--- __test__/manipulateList.test.js
+++ __test__/manipulateList.test.js
@@ -3,6 +3,7 @@
  */
 
 import List from '../src/manipulateList.js';
+import Task from '../src/task.js';
 
 const innerHtml = `
  <div class="oneMoreTask">
@@ -17,7 +18,7 @@ const tasksUL = document.querySelector('.tasks_list');
 const addItemField = document.getElementById('add_item');
 
 describe('#addToList is working properly', () => {
-  test('if new task is properly created', () => {
+  test('if new Task is properly created', () => {
     // Arrange
     const currentList = new List();
 
@@ -69,6 +70,66 @@ describe('#addToList is working properly', () => {
     // Assert
     expect(lastAddedChild.tagName).toBe('LI');
     expect(lastAddedChild.id).toBe(`${UlPreviousLenght}`);
+    expect(lastAddedChild.children[1].innerText).toBe('New Task');
+    expect(lastAddedChild.children[0].checked).toBe(false);
+  });
+});
+
+describe('#addToList is working properly, passing a fake Storage.key as attribute', () => {
+  test('if new task is properly created', () => {
+    // Arrange
+
+    // the List constructor has a default attribute storageKey,
+    // if a fake name is passed will return an empty array
+    const currentList = new List('fakeStorageKeyName');
+
+    // Act: call #addToList
+
+    // currentList() returns the const newTask
+    // (the Object being pushed into the array)
+    const task = currentList.addToList(addItemField.value);
+
+    // Assert
+    expect(typeof task).toBe('object');
+    expect(task.constructor.name).toBe('Task');
+    expect(task).toEqual({ index: 0, description: 'New Task', done: false });
+  });
+
+  test('if list array contains only one Task object', () => {
+    // Arrange
+    const currentList = new List('fakeStorageKeyName'); // intialized list will equal to empty array
+
+    // Act: call #addToList
+    currentList.addToList(addItemField.value);
+
+    // Assert
+    expect(currentList.getList()).toHaveLength(1); // check if only one task was added
+
+    // Arrange: store object from array to variable to check its properties
+    const objectInArray = currentList.getList()[0];
+
+    // Assert: properties of the newly added object
+    expect(typeof objectInArray).toBe('object');
+    expect(objectInArray.constructor.name).toEqual('Task');
+    expect(objectInArray).toEqual({ index: 0, description: 'New Task', done: false });
+  });
+
+  test('if one <li> element is appended to list container', () => {
+    // Arrange
+    const currentList = new List('fakeStorageKeyName');
+
+    // Act: call #addToList
+    currentList.addToList(addItemField.value);
+
+    // Assert
+    expect(currentList.getList()).toHaveLength(1); // check if only one task was added
+
+    // Act: get the newly added object after calling #addToList
+    const lastAddedChild = tasksUL.children[0];
+
+    // Assert
+    expect(lastAddedChild.tagName).toBe('LI');
+    expect(lastAddedChild.id).toEqual('0');
     expect(lastAddedChild.children[1].innerText).toBe('New Task');
     expect(lastAddedChild.children[0].checked).toBe(false);
   });
