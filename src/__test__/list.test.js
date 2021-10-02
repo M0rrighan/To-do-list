ae936d22b1cc6c5fc63ccbf5d4583dde568431df
adewunmi <shonibareadewunmicomfort@gmail.com>
2021-10-02 21:19:13 +0100

    Mock the HTML elements to test DOM manipulation functions

--- src/__test__/list.test.js
+++ src/__test__/list.test.js
@@ -1,47 +1,78 @@
+/**
+ * @jest-environment jsdom
+ */
 import Manipulation from '../manipulateList';
 import Storage from '../storage';
 
 describe('List', () => {
-  const todos = [{ id: 1, description: 'Description', done: false }];
+  document.body.innerHTML = `<div class="list_container">
+  <div class="title">
+    <p>Today's To Do</p>
+    <i class="material-icons">autorenew</i>
+  </div>
+  <div class="oneMoreTask">
+    <input type="text" name="add_item" id="add_item" class="add_item" placeholder="Add to your list...">
+    <i class="material-icons">keyboard_return</i>
+  </div>
+  <ul class="tasks_list"></ul>
+  <div class="clearBtn">Clear all completed</div>
+</div>`;
+
+  global.localStorage = {
+    state: {},
+    setItem(key, item) {
+      this.state[key] = item;
+    },
+    getItem(key) {
+      return this.state[key];
+    },
+  };
+  let tasks;
   beforeAll(() => {
-    Storage.existInStorage = jest.fn().mockReturnValue(false);
-    Storage.saveAndUpdate = jest.fn().mockReturnValue(todos);
-    Storage.readFromStorage = jest.fn().mockReturnValue(todos);
+    tasks = new Manipulation('ToDoList');
+
+    tasks.addToList('Test 1');
   });
 
   afterAll(() => {
-    Storage.existInStorage.mockRestore();
-    Storage.saveAndUpdate.mockRestore();
-    Storage.readFromStorage.mockRestore();
+    localStorage.removeItem('ToDoList');
   });
+
   describe('Change Status', () => {
     test('should change status to completed', () => {
-      const tasks = new Manipulation();
-      tasks.tasksList = todos;
-
       const editedTasks = tasks.changeStatusDone(0);
+      Storage.saveAndUpdate(tasks.tasksList);
       expect(editedTasks[0].done).toBe(true);
+    });
+
+    test('should check checkbox ', () => {
+      const { checked } = document.querySelector('ul li input');
+      expect(checked).toBe(true);
     });
   });
 
   describe('Edit task', () => {
     test('should edit task description', () => {
-      const tasks = new Manipulation();
-      tasks.tasksList = todos;
-
       const editedTasks = tasks.editDescription(0, 'Updated Desc');
       expect(editedTasks[0].description).toBe('Updated Desc');
+    });
+
+    test('should update task description in the DOM', () => {
+      Storage.saveAndUpdate(tasks.tasksList);
+      const desc = document.querySelector('ul li p').innerText;
+      expect(desc).toBe('Updated Desc');
     });
   });
 
   describe('Change Status', () => {
     test('should clear all completed tasks', () => {
-      const tasks = new Manipulation();
-      const customTodos = [{ id: 1, description: 'Description', done: true }];
-      tasks.tasksList = customTodos;
-
       tasks.removeAllDone();
       expect(tasks.getList().length).toBe(0);
+    });
+
+    test('should clear all completed tasks from DOM', () => {
+      const li = document.querySelectorAll('ul li');
+      expect(li.length).toBe(0);
     });
   });
 });
